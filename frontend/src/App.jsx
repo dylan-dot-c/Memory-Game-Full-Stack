634418b5de01e20487c6ccf7f58e7b88306dcd84
Dylan Heslop <heslopd23@gmail.com>
2023-09-06 16:02:00 -0400

    updated to HashRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import { useEffect, useState } from "react";
-
-import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LeaderBoard from "./pages/LeaderBoard";
 import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ToastContainer theme="dark" />
       <Routes>
         <Route path="/leaderboard" element={<LeaderBoard />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
