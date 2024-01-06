cf01d54ceb82117e94c13a11f4810b321ee5fc96
Dylan Heslop <heslopd23@gmail.com>
2024-01-06 11:02:33 -0500

    added .env.example and fixed some config

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,27 +1,27 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    username: "root",
-    password: "meatloaf",
-    database: "memory_game",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: 3306,
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: process.env.USER,
-    password: process.env.PROD_PASSWORD,
-    database: process.env.PROD_DATABASE,
-    host: process.env.PROD_HOST,
-    dialect: process.env.PROD_DIALECT,
-    port: process.env.PROD_PORT,
-  },
+    development: {
+        username: process.env.DEV_USERNAME,
+        password: process.env.DEV_PASSWORD,
+        database: process.env.DEV_DATABASE,
+        host: "127.0.0.1",
+        dialect: "mysql",
+        port: 3306,
+    },
+    test: {
+        username: "root",
+        password: null,
+        database: "database_test",
+        host: "127.0.0.1",
+        dialect: "mysql",
+    },
+    production: {
+        username: process.env.USER,
+        password: process.env.PROD_PASSWORD,
+        database: process.env.PROD_DATABASE,
+        host: process.env.PROD_HOST,
+        dialect: process.env.PROD_DIALECT,
+        port: process.env.PROD_PORT,
+    },
 };
